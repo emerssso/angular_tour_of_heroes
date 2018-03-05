e681c71d9138c93be44ef4dde33ee7acf29e4eb0
Conner Kasten <emerssso@gmail.com>
2018-03-04 18:41:25 -0800

    Get Hero details from class

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,22 @@
 import 'package:angular/angular.dart';
 
+class Hero {
+  final int id;
+  String name;
+
+  Hero(this.id, this.name);
+}
+
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  template: '''
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div><label>name: </label>{{hero.name}}</div>
+  ''',
 )
 class AppComponent {
-  var name = 'Angular';
+  final title = 'Tour of Heroes';
+  var hero = new Hero(1, 'Windstorm');
 }
