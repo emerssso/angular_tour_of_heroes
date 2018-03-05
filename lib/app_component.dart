c3e384a2cf3c8f19c4b2aa39e616c7da98787be7
Conner Kasten <emerssso@gmail.com>
2018-03-04 18:49:05 -0800

    Make Hero name editable

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,22 +1,27 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+
+@Component(
+  selector: 'my-app',
+  directives: const [formDirectives],
+  template: '''
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+  ''',
+)
+class AppComponent {
+  final title = 'Tour of Heroes';
+  var hero = new Hero(1, 'Windstorm');
+}
 
 class Hero {
   final int id;
   String name;
 
   Hero(this.id, this.name);
-}
-
-@Component(
-  selector: 'my-app',
-  template: '''
-    <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div><label>name: </label>{{hero.name}}</div>
-  ''',
-)
-class AppComponent {
-  final title = 'Tour of Heroes';
-  var hero = new Hero(1, 'Windstorm');
 }
